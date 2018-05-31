fc97e571a3feabdee39a24e7dbfa8cc45e6d1890
Lauren West <laurenwest12@gmail.com>
2018-05-31 00:25:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph")
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob")
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(){
+  return [b, ...kittens]
+}
